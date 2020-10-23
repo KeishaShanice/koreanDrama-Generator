9bf84df15bea804d75c9324dbe888e5b0c95921f
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2020-10-22 22:39:33 -0500

    added style

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ function generateDrama() {
     
     let drama = document.getElementById('drama');
     
-    let dramaList = ['Itaewon Class', 'The Master\'s Sun', 'Secret Garden', 'The Greatest Love', 'Coffee Prince', 'IRIS', 'Boys Over Flowers', 'You\'re All Surrounded', 'Big', 'Goblin', 'Heirs', 'Hospital Playlist', 'It\'s Okay, That\'s Love', 'Signal', 'King2Hearts', 'Crash Landing on You', 'Vagabond', 'Fight My Way', 'Weightlifthing Fairy Kim Bokjoo', 'Chief Kim', 'Mr. Sunshine', 'Falling for Innocence', 'Hi Bye Mama', 'Kill Me Heal Me', 'Korean Odyssey', 'Cruel City', 'Bad Guys']; 
+    let dramaList = ['Itaewon Class', 'The Master\'s Sun', 'Secret Garden', 'The Greatest Love', 'Coffee Prince', 'IRIS', 'Boys Over Flowers', 'You\'re All Surrounded', 'Big', 'Goblin', 'Heirs', 'Hospital Playlist', 'It\'s Okay, That\'s Love', 'Signal', 'King2Hearts', 'Crash Landing on You', 'Vagabond', 'Fight My Way', 'Weightlifting Fairy Kim Bokjoo', 'Chief Kim', 'Mr. Sunshine', 'Falling for Innocence', 'Hi Bye Mama', 'Kill Me Heal Me', 'Korean Odyssey', 'Cruel City', 'Bad Guys']; 
     
     drama.innerText = dramaList[Math.floor(Math.random() * dramaList.length)];
     
