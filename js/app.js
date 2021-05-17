f6a4e41879d80ee6ccfc96b5d1183eae9dd3c60d
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2021-05-17 10:15:53 -0500

    added 6 dramas

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ function generateDrama() {
     
     let drama = document.getElementById('drama');
     
-    let dramaList = ['Itaewon Class', 'The Master\'s Sun', 'Secret Garden', 'The Greatest Love', 'Coffee Prince', 'IRIS', 'Boys Over Flowers', 'You\'re All Surrounded', 'Big', 'Goblin', 'Heirs', 'Hospital Playlist', 'It\'s Okay, That\'s Love', 'Signal', 'King2Hearts', 'Crash Landing on You', 'Vagabond', 'Fight My Way', 'Weightlifting Fairy Kim Bokjoo', 'Chief Kim', 'Mr. Sunshine', 'Falling for Innocence', 'Hi Bye Mama', 'Kill Me Heal Me', 'Korean Odyssey', 'Cruel City', 'Bad Guys', 'Romance is a Bonus Book', 'What\'s wrong with Secretary Kim','Prison Playbook', 'Start Up', "Cinderella and the Four Knights", 'The World of the Married', 'Sky Castle', 'Reply 1988', 'Mr. Queen', '100 Days My Prince']; 
+    let dramaList = ['Itaewon Class', 'The Master\'s Sun', 'Secret Garden', 'The Greatest Love', 'Coffee Prince', 'IRIS', 'Boys Over Flowers', 'You\'re All Surrounded', 'Big', 'Goblin', 'Heirs', 'Hospital Playlist', 'It\'s Okay, That\'s Love', 'Signal', 'King2Hearts', 'Crash Landing on You', 'Vagabond', 'Fight My Way', 'Weightlifting Fairy Kim Bokjoo', 'Chief Kim', 'Mr. Sunshine', 'Falling for Innocence', 'Hi Bye Mama', 'Kill Me Heal Me', 'Korean Odyssey', 'Cruel City', 'Bad Guys', 'Romance is a Bonus Book', 'What\'s wrong with Secretary Kim','Prison Playbook', 'Start Up', "Cinderella and the Four Knights", 'The World of the Married', 'Sky Castle', 'Reply 1988', 'Mr. Queen', '100 Days My Prince', 'Hello Me']; 
     
     drama.innerText = dramaList[Math.floor(Math.random() * dramaList.length)];
     
